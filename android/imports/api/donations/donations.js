e462aa0dd8a62b48a52ee4108c7f753e22a1ddbb
Lucian Stuparu <lucianstuparu@gmail.com>
2016-10-22 21:45:31 +0300

    - added donations schema

--- android/imports/api/donations/donations.js
+++ android/imports/api/donations/donations.js
@@ -3,3 +3,10 @@ import { Mongo } from 'meteor/mongo';
 import { check } from 'meteor/check';
 
 export const Donations = new Mongo.Collection('donations');
+
+Donations.schema = new SimpleSchema({
+  userId: {type: String, regEx: SimpleSchema.RegEx.Id},
+  date: {type: Date},
+  address: {type: String},
+  center: {type: String}
+});
